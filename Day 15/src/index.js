d4b43fe738daa5ea3b944478ce33c3d5cb67df29
iam-krj <75441878+iam-krj@users.noreply.github.com>
2021-07-17 22:34:39 +0530

    Add files via upload
    React For the first time.

---
+++ Day 15/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+  <h1>HII WORLD!</h1> 
+  <h2>Day 15</h2>
+  
+  <h2>Isme kuch nahi krna tha </h2>
+  </div>
+  , document.getElementById("root")
+  
+  
+)
