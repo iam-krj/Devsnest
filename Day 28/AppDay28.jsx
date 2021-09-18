77ab1db367c72aec7e886fe1c2390324410db2b6
Krishna Raj <75441878+iam-krj@users.noreply.github.com>
2021-09-18 23:16:56 +0530

    Add files via upload

---
+++ Day 28/AppDay28.jsx
@@ -0,0 +1,59 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Styleday28.css"
+import { useState, useEffect } from "react";
+
+function App() {
+  const [place, setPlace] = useState("");
+  const [placeData, setPlaceData] = useState({});
+  const updatePaceData = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=b02b0278db2646f19a7142812212707&q=${place}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPlaceData(data);
+      });
+  }
+  return (
+    <div className="app">
+      <div className="container">
+          <div className="col-12 form">
+            <input type="text" value={place} onChange={(e) => {
+                setPlace(e.target.value);
+              }}
+            />
+            <button className="btn btn-primary" onClick={updatePaceData}>Submit</button>
+          </div>
+          <div className="offset-md-4 col-12 col-md-4 weather">
+            <div className="card">
+              {placeData.location ? (
+                <div>
+                  <img src={placeData.current.condition.icon} alt="" />
+                  <div className="temp">{placeData.current.temp_c}°C</div>
+                  <div className="desc">{placeData.current.condition.text}</div>
+                  <div className="place">{placeData.location.name}</div>
+                  <div className="container">
+                    <div className="row whp">
+                      <div className="col">
+                        <div className="title">Humidity</div>
+                        <div className="data">
+                          {placeData.current.humidity}{" "}
+                          <span className="unit">%</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <h2>Place not found</h2>
+              )}
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ Day 28/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./Appday28";
+ReactDOM.render(
+    
+        
+            <App />
+        
+    ,
+    document.getElementById("root")
+  );
