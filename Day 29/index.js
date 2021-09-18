5cd05463054afdd8b89b38b84fc772fe821f4cca
Krishna Raj <75441878+iam-krj@users.noreply.github.com>
2021-09-18 23:08:56 +0530

    Add files via upload

---
+++ Day 29/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./Appday29";
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
